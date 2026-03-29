c4a5980472e2fdf195c89b8037e0fd1791e68789
tkonov91 <110422186+tkonov91@users.noreply.github.com>
2026-03-30 01:16:20 +0300

    second commit

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import requests
+from collections import Counter
 
 def get_text(url):
     response = requests.get(url)
     return response.text
 
-def count_word_frequencies(url, word):
+def count_word_frequencies(url, words_to_count):
     text = get_text(url)
-    words = text.split()
-    count = 0
-    for w in words:
-        if w == word:
-            count += 1
-    return count
+    word_counts = Counter(text.split())
+    return {word: word_counts[word] for word in words_to_count}
 
 def main():
     words_file = "words.txt"
     url = "https://eng.mipt.ru/why-mipt/"
 
     words_to_count = []
-    with open(words_file, 'r') as file:
+    with open(words_file, 'r', encoding='utf-8') as file:
         for line in file:
             word = line.strip()
             if word:
                 words_to_count.append(word)
 
-    frequencies = {}
-    for word in words_to_count:
-        frequencies[word] = count_word_frequencies(url, word)
-    
+    frequencies = count_word_frequencies(url, words_to_count)
     print(frequencies)
 
 if __name__ == "__main__":
